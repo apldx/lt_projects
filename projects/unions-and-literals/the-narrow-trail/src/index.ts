b9e5367f8435f6288f02be0b0f362546acb79703
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-01 10:39:37 -0700

    Updates

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,39 @@
 export function runCommands() {
-	// Declare your variables and runtime logic here! ✨
+  let nextSupply: 'food' | 'water' | undefined;
+  let food = 5;
+  let water = 5;
+
+  for (let time = 1; time <= 7; time += 1) {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    console.log(time, roll, food, water);
+    if (roll == 1) {
+      nextSupply = 'food';
+    } else if (roll == 2) {
+      nextSupply = 'water';
+    } else {
+      if (nextSupply) {
+        if (nextSupply === 'food') {
+          food += roll;
+        } else {
+          water += roll;
+        }
+        nextSupply = undefined;
+      } else {
+        if (roll % 2 == 0) {
+          nextSupply = 'food';
+        } else {
+          nextSupply = 'water';
+        }
+      }
+    }
+
+    food -= 1;
+    water -= 1;
+    if (food == 0 || water == 0) {
+      console.log('Returning false');
+      return false;
+    }
+  }
+
+  return true;
 }
