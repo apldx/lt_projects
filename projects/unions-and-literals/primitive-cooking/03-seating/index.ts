c759edfd78ce127af39e8ae975d760d158208205
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-01 09:29:06 -0700

    Updates

--- projects/unions-and-literals/primitive-cooking/03-seating/index.ts
+++ projects/unions-and-literals/primitive-cooking/03-seating/index.ts
@@ -1,43 +1,44 @@
 // Please fill in any missing type annotations here...
-const headOfTable = "Me!";
-let adjacentLeft;
-let adjacentRight;
-let furtherLeft;
-let furtherRight;
+const headOfTable = 'Me!';
+type Guest = 'Susie' | 'Tommy' | 'Angelica' | 'Chuckie' | 'Kimi' | 'Timmy';
+let adjacentLeft: Guest;
+let adjacentRight: Guest;
+let furtherLeft: Guest | undefined;
+let furtherRight: Guest | undefined;
 
 // I always invite Susie and Tommy! ♥
 if (Math.random() > 0.5) {
-	adjacentLeft = "Susie";
-	adjacentRight = "Tommy";
+  adjacentLeft = 'Susie';
+  adjacentRight = 'Tommy';
 } else {
-	adjacentLeft = "Tommy";
-	adjacentRight = "Susie";
+  adjacentLeft = 'Tommy';
+  adjacentRight = 'Susie';
 }
 
 // I invite Angelica about half of the time. We're not as close as Susie and Tommy. It's a long story.
 // I try to fill `furtherLeft` first...
 if (Math.random() > 0.5) {
-	furtherLeft = "Angelica";
+  furtherLeft = 'Angelica';
 }
 
 // Same with Chuckie. I like them, but do I *really* like hanging out with them? Only sometimes.
 // ...then after that `furtherRight`
 if (Math.random() > 0.5) {
-	if (furtherLeft) {
-		furtherRight = "Chuckie";
-	} else {
-		furtherLeft = "Chuckie";
-	}
+  if (furtherLeft) {
+    furtherRight = 'Chuckie';
+  } else {
+    furtherLeft = 'Chuckie';
+  }
 }
 
 // If I invited Angelica but not Chuckie, I'll invite Kimi. They get along well with Angelica but not Chuckie.
-if (furtherLeft === "Angelica" && furtherRight !== "Chuckie") {
-	furtherRight = "Kimi";
+if (furtherLeft === 'Angelica' && furtherRight !== 'Chuckie') {
+  furtherRight = 'Kimi';
 }
 
 // If I invited Chuckie but not Angelica, I'll invite Timmy. They get along well with Chuckie but not Angelica.
-if (furtherLeft === "Chuckie") {
-	furtherRight = "Timmy";
+if (furtherLeft === 'Chuckie') {
+  furtherRight = 'Timmy';
 }
 
 console.log(`At the head of the table is... ${headOfTable}`);
@@ -45,7 +46,7 @@ console.log(`At the head of the table is... ${headOfTable}`);
 console.log(`Adjacent to the left is: ${adjacentLeft}`);
 console.log(`Adjacent to the right is: ${adjacentRight}`);
 
-console.log(`Further down on the left is: ${adjacentLeft ?? "nobody"}`);
-console.log(`Further down on the right is: ${adjacentRight ?? "nobody"}`);
+console.log(`Further down on the left is: ${adjacentLeft ?? 'nobody'}`);
+console.log(`Further down on the right is: ${adjacentRight ?? 'nobody'}`);
 
 export {};
