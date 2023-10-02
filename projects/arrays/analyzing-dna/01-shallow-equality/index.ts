b20f18a1211155bbc565d5b8b071aad7c8c02434
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-02 14:40:54 -0700

    Completed analyzing-dna

--- projects/arrays/analyzing-dna/01-shallow-equality/index.ts
+++ projects/arrays/analyzing-dna/01-shallow-equality/index.ts
@@ -1,2 +1,14 @@
-// Write your shallowEquality function here! ✨
-// You'll need to export it so the tests can run it.
+export const shallowEquality = (a: string[], b: string[]) => {
+  if (a.length != b.length) {
+    return false;
+  }
+
+  let equal = true;
+  a.forEach((el, idx) => {
+    if (el !== b[idx]) {
+      equal = false;
+    }
+  });
+
+  return equal;
+};
--- projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,24 @@
-// Write your deepDifferences function here! ✨
-// You'll need to export it so the tests can run it.
+export const deepDifferences = (a: string[][], b: string[][]) => {
+  if (a.length !== b.length) {
+    return undefined;
+  }
+
+  const result: ((string | undefined)[] | undefined)[] = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i].length !== b[i].length) {
+      result.push(undefined);
+    } else {
+      const these_results: (string | undefined)[] = [];
+      for (let j = 0; j < a[i].length; j++) {
+        if (a[i][j] !== b[i][j]) {
+          these_results.push(undefined);
+        } else {
+          these_results.push(a[i][j]);
+        }
+      }
+      result.push(these_results);
+    }
+  }
+
+  return result;
+};
