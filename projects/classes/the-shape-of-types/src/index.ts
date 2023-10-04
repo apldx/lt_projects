724d4a5256d803f634619c5ecc5eca5d6a5ecc67
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-04 12:03:52 -0700

    Completed the-shape-of-types

--- projects/classes/the-shape-of-types/src/index.ts
+++ projects/classes/the-shape-of-types/src/index.ts
@@ -1,2 +1,78 @@
-// Write your classes here! ✨
-// You'll need to export them so the tests can run them.
+export interface ConsumedOpponent {
+  name: string;
+  evil: boolean;
+  power: number;
+}
+
+export abstract class Horror {
+  abstract name: string;
+  #consumedOpponents: ConsumedOpponent[] = [];
+
+  doBattle(opponent: Horror) {
+    if (this.getPower() >= opponent.getPower()) {
+      this.consume(opponent);
+    }
+  }
+
+  getPower() {
+    let power = 0;
+    for (const opponent of this.#consumedOpponents) {
+      power += this.getPowerFrom(opponent);
+    }
+    power += this.#consumedOpponents.length;
+    return power;
+  }
+
+  consume(opponent: Horror) {
+    const consumedOpponent: ConsumedOpponent = {
+      name: opponent.name,
+      evil: opponent.isEvil(),
+      power: opponent.getPower()
+    };
+    this.#consumedOpponents.push(consumedOpponent);
+  }
+
+  protected abstract getPowerFrom(consumedOpponent: ConsumedOpponent): number;
+
+  protected abstract isEvil(): boolean;
+}
+
+export class Demon extends Horror {
+  name: string;
+
+  constructor() {
+    super();
+    this.name = 'Demon';
+  }
+
+  getPowerFrom(consumedOpponent: ConsumedOpponent) {
+    return consumedOpponent.evil
+      ? 0.5 * consumedOpponent.power
+      : 2 * consumedOpponent.power;
+  }
+
+  isEvil() {
+    return true;
+  }
+}
+
+export class Sorcerer extends Horror {
+  name: string;
+  #evil: boolean;
+
+  constructor(name: string, evil: boolean) {
+    super();
+    this.name = name;
+    this.#evil = evil;
+  }
+
+  getPowerFrom(consumedOpponent: ConsumedOpponent) {
+    return consumedOpponent.evil == this.#evil
+      ? 2 * consumedOpponent.power
+      : consumedOpponent.power;
+  }
+
+  isEvil() {
+    return this.#evil;
+  }
+}
--- projects/classes/the-shape-of-types/src/solution.ts
+++ projects/classes/the-shape-of-types/src/solution.ts
@@ -1,67 +1,67 @@
 interface Consumed {
-	evil: boolean;
-	name: string;
-	power: number;
+  evil: boolean;
+  name: string;
+  power: number;
 }
 
 export abstract class Horror {
-	#consumed: Consumed[] = [];
+  #consumed: Consumed[] = [];
 
-	abstract readonly name: string;
+  abstract readonly name: string;
 
-	#consume(opponent: Horror) {
-		this.#consumed.push({
-			evil: opponent.isEvil(),
-			name: opponent.name,
-			power: opponent.getPower(),
-		});
-	}
+  #consume(opponent: Horror) {
+    this.#consumed.push({
+      evil: opponent.isEvil(),
+      name: opponent.name,
+      power: opponent.getPower()
+    });
+  }
 
-	doBattle(opponent: Horror) {
-		if (this.getPower() >= opponent.getPower()) {
-			this.#consume(opponent);
-		}
-	}
+  doBattle(opponent: Horror) {
+    if (this.getPower() >= opponent.getPower()) {
+      this.#consume(opponent);
+    }
+  }
 
-	getPower() {
-		return this.#consumed.reduce(
-			(previous, consumed) => previous + this.getPowerFrom(consumed),
-			this.#consumed.length
-		);
-	}
+  getPower() {
+    return this.#consumed.reduce(
+      (previous, consumed) => previous + this.getPowerFrom(consumed),
+      this.#consumed.length
+    );
+  }
 
-	protected abstract getPowerFrom(consumed: Consumed): number;
+  protected abstract getPowerFrom(consumed: Consumed): number;
 
-	protected abstract isEvil(): boolean;
+  protected abstract isEvil(): boolean;
 }
 
 export class Demon extends Horror {
-	readonly name = "Demon";
+  readonly name = 'Demon';
 
-	getPowerFrom(consumed: Consumed) {
-		return consumed.evil ? consumed.power / 2 : consumed.power * 2;
-	}
+  getPowerFrom(consumed: Consumed) {
+    return consumed.evil ? consumed.power / 2 : consumed.power * 2;
+  }
 
-	isEvil() {
-		return true;
-	}
+  isEvil() {
+    return true;
+  }
 }
 
 export class Sorcerer extends Horror {
-	readonly name: string;
-	#evil: boolean;
+  readonly name: string;
+  #evil: boolean;
 
-	constructor(name: string, evil: boolean) {
-		super();
-		this.name = name;
-		this.#evil = evil;
-	}
+  constructor(name: string, evil: boolean) {
+    super();
+    this.name = name;
+    this.#evil = evil;
+  }
 
-	getPowerFrom(consumed: Consumed) {
-		return consumed.evil === this.#evil ? consumed.power * 2 : consumed.power;
-	}
+  getPowerFrom(consumed: Consumed) {
+    return consumed.evil === this.#evil ? consumed.power * 2 : consumed.power;
+  }
 
-	isEvil() {
-		return this.#evil;
-	}
+  isEvil() {
+    return this.#evil;
+  }
 }
