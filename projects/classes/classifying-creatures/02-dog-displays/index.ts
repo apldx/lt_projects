982eacd7a264cf299c8c3828a7ff7ccddf717491
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-04 11:17:45 -0700

    Completed classifying-creatures

--- projects/classes/classifying-creatures/02-dog-displays/index.ts
+++ projects/classes/classifying-creatures/02-dog-displays/index.ts
@@ -1,2 +1,19 @@
-// Write your class and interface here! ✨
-// You'll need to export it so the tests can run it.
+interface PuppyInTheWindow {
+  colors: string[];
+  furriness: number;
+  owner: string | undefined;
+}
+
+export class Puppy implements PuppyInTheWindow {
+  colors: string[];
+  furriness: number;
+  owner: string | undefined;
+  constructor(colors: string[], furriness: number) {
+    this.colors = colors;
+    this.furriness = furriness;
+  }
+
+  adopt(owner: string) {
+    this.owner = owner;
+  }
+}
