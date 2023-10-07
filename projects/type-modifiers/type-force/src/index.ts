921574c6ba6d3ea8b81993f6c7fed232b79fd5fa
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-07 16:15:11 -0700

    Completed type-force

--- projects/type-modifiers/type-force/src/index.ts
+++ projects/type-modifiers/type-force/src/index.ts
@@ -1,41 +1,68 @@
-// Write your duel function and types below! ✨
-// You'll need to export duel so the tests can run it.
+interface Character {
+  flying: boolean;
+  name: string;
+  power: number;
+  toughness: number;
+}
 
 const mutationsLibrary = {
-	energy: (hero) => {
-		hero.power *= 1.25;
-		hero.flying = true;
-	},
-	healing: (hero) => {
-		hero.toughness *= 2;
-	},
-	luck: (hero) => {
-		hero.power *= 1.25;
-		hero.toughness *= 1.25;
-	},
-	flight: (hero) => {
-		hero.flying = true;
-	},
-	strength: (hero) => {
-		hero.power *= 2;
-	},
-	wings: (hero) => {
-		hero.flying = true;
-		hero.toughness *= 0.9;
-	},
+  energy: (hero: Character) => {
+    hero.power *= 1.25;
+    hero.flying = true;
+  },
+  healing: (hero: Character) => {
+    hero.toughness *= 2;
+  },
+  luck: (hero: Character) => {
+    hero.power *= 1.25;
+    hero.toughness *= 1.25;
+  },
+  flight: (hero: Character) => {
+    hero.flying = true;
+  },
+  strength: (hero: Character) => {
+    hero.power *= 2;
+  },
+  wings: (hero: Character) => {
+    hero.flying = true;
+    hero.toughness *= 0.9;
+  }
 };
 
-function createCharacter(name, mutations) {
-	const character = {
-		flying: false,
-		name,
-		power: 1,
-		toughness: 1,
-	};
+type Mutation = keyof typeof mutationsLibrary;
 
-	for (const mutation of mutations) {
-		mutationsLibrary[mutation](character);
-	}
-
-	return character;
+interface Fighter {
+  name: string;
+  mutations: Mutation[];
 }
+
+const createCharacter = (name: string, mutations: Mutation[]) => {
+  const character: Character = {
+    flying: false,
+    name,
+    power: 1,
+    toughness: 1
+  };
+
+  for (const mutation of mutations) {
+    mutationsLibrary[mutation](character);
+  }
+
+  return character;
+};
+
+interface Characters {
+  [key: string]: Character;
+}
+
+export const duel = (good: Fighter, bad: Fighter) => {
+  const fighters: Characters = {};
+  fighters.hero = createCharacter(good.name, good.mutations);
+  fighters.villain = createCharacter(bad.name, bad.mutations);
+  const winner =
+    fighters.hero.power / fighters.villain.toughness >=
+    fighters.villain.power / fighters.hero.toughness
+      ? 'hero'
+      : 'villain';
+  return [winner, fighters[winner]] as const;
+};
