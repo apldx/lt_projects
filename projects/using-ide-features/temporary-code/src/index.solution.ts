727ebfcc648b8ce76f5b6078e0293afc181a417b
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-09 17:17:11 -0700

    Completed temporary-code

--- projects/using-ide-features/temporary-code/src/index.solution.ts
+++ projects/using-ide-features/temporary-code/src/index.solution.ts
@@ -1,28 +1,26 @@
-import { criticizePerson } from "./criticizePerson.solution";
-import { Person } from "./Person.solution";
-
-// Refactor here! ✨
+import { criticizePerson } from './criticizePerson.solution';
+import { Person } from './Person.solution';
 
 function createPerson(name: string, quote: string): Person {
-	return { name, quote, type: "person" };
+  return { name, quote, type: 'person' };
 }
 
 const examplePeople = [
-	createPerson("archeologist", "insert archeologist quote"),
-	createPerson("historian", "insert historian quote"),
-	createPerson("programmer", "#shipit"),
+  createPerson('archeologist', 'insert archeologist quote'),
+  createPerson('historian', 'insert historian quote'),
+  createPerson('programmer', '#shipit')
 ];
 
 function describePerson(person: Person) {
-	const { name, type, quote } = person;
+  const { name, type, quote } = person;
 
-	return `The ${name} ${type} goes: ${quote}!`;
+  return `The ${name} ${type} goes: ${quote}!`;
 }
 
 export function announceExamplePeople() {
-	for (const person of examplePeople) {
-		console.log(describePerson(person));
+  for (const person of examplePeople) {
+    console.log(describePerson(person));
 
-		criticizePerson(person);
-	}
+    criticizePerson(person);
+  }
 }
--- projects/using-ide-features/temporary-code/src/index.ts
+++ projects/using-ide-features/temporary-code/src/index.ts
@@ -1,27 +1,26 @@
-// Refactor here! ✨
+import { Person } from './Person';
+import { criticizePerson } from './criticizePerson';
 
-function tempCreatePersonTodoRefactorSoon(name: string, quote: string) {
-	return { name, quote, type: "person" };
+function createPerson(name: string, quote: string): Person {
+  return { name, quote, type: 'person' };
 }
 
 const examplePeople = [
-	tempCreatePersonTodoRefactorSoon("archeologist", "insert archeologist quote"),
-	tempCreatePersonTodoRefactorSoon("historian", "insert historian quote"),
-	tempCreatePersonTodoRefactorSoon("programmer", "#shipit"),
+  createPerson('archeologist', 'insert archeologist quote'),
+  createPerson('historian', 'insert historian quote'),
+  createPerson('programmer', '#shipit')
 ];
 
-function describePerson(person) {
-	const { name, type, quote } = person;
+function describePerson(person: Person) {
+  const { name, type, quote } = person;
 
-	return "The " + name + " " + type + " goes: " + quote + "!";
+  return `The ${name} ${type} goes: ${quote}!`;
 }
 
 export function announceExamplePeople() {
-	for (const person of examplePeople) {
-		console.log(describePerson(person));
+  for (const person of examplePeople) {
+    console.log(describePerson(person));
 
-		if (person.quote.indexOf("#") !== -1) {
-			console.log(`\t${person.name} should stop going on Twitter so much...`);
-		}
-	}
+    criticizePerson(person);
+  }
 }
