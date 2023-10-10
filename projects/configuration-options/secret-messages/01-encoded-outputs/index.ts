198aeb2d0c3ac74b0bc8e53ab1da642e362632c1
Ajay Khanna <appliedstochastics@gmail.com>
2023-10-09 19:19:51 -0700

    Completed secret-messages

--- projects/configuration-options/secret-messages/01-encoded-outputs/index.ts
+++ projects/configuration-options/secret-messages/01-encoded-outputs/index.ts
@@ -1,11 +1,11 @@
 // Write your types here! ✨
 
 export function encodeMessage(message: string) {
-	let output = "";
+  let output = '';
 
-	for (let i = 0; i < message.length; i += 1) {
-		output += String.fromCharCode(message.codePointAt(i)! + i + message.length);
-	}
+  for (let i = 0; i < message.length; i += 1) {
+    output += String.fromCharCode(message.codePointAt(i)! + i + message.length);
+  }
 
-	return output;
+  return output;
 }
